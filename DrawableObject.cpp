cab930d403351383723f8292b099114c3171722e
Fughilli <kbalke@ucla.edu>
2014-07-23 13:19:45 -0700

    Broke drawable object implementation out into separate header/source files

---
+++ DrawableObject.cpp
@@ -0,0 +1,20 @@
+#include "DrawableObject.h"
+
+void MeshObject3d::draw(Camera3d& camera, DisplayInterface& _di)
+{
+    draw(camera, _di, Vector3d::one);
+}
+
+void MeshObject3d::draw(Camera3d& camera, DisplayInterface& _di, const Vector3d& directLight)
+{
+    Triangle2d temp;
+    for(int i = 0; i < numTris; i++)
+    {
+        Triangle3d modifiedMeshTri = mesh[i].transform(oscale, orientation, position);
+        if(modifiedMeshTri.normal().dot(camera.forward.rotate(camera.getRot())) < 0)
+        {
+            temp = projectTri3d(camera, modifiedMeshTri);
+            _di.drawTri(temp, grayscaleToColor((directLight.unit().dot(modifiedMeshTri.normal()) + 1)*128), true);
+        }
+    }
+}
---
+++ DrawableObject.h
@@ -0,0 +1,22 @@
+#ifndef DRAWABLE_OBJECT_H
+#define DRAWABLE_OBJECT_H
+
+#include "Camera3D.h"
+#include "DisplayInterface.h"
+
+class DrawableObject3d : public Object3d
+{
+public:
+    virtual void draw(Camera3d& camera, DisplayInterface& _di) = 0;
+};
+
+class MeshObject3d : public DrawableObject3d
+{
+public:
+    Triangle3d * mesh;
+    uint16_t numTris;
+    virtual void draw(Camera3d& camera, DisplayInterface& _di);
+    virtual void draw(Camera3d& camera, DisplayInterface& _di, const Vector3d& directLight);
+};
+
+#endif // DRAWABLE_OBJECT_H
